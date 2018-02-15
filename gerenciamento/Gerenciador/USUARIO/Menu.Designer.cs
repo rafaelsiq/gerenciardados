94018d7bf9f3caf0fa09304ac50e8d2fb58ad880
rafaelsiq <rafaelsqf2@gmail.com>
2018-02-15 17:03:47 -0200

    15022018

--- gerenciamento/Gerenciador/USUARIO/Menu.Designer.cs
+++ gerenciamento/Gerenciador/USUARIO/Menu.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.pictureBox_faixa = new System.Windows.Forms.PictureBox();
             this.label_razaosocial = new System.Windows.Forms.Label();
+            this.panel_cliente = new System.Windows.Forms.Panel();
+            this.button_clientes = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_faixa)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,7 +40,7 @@
             this.pictureBox_faixa.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox_faixa.Location = new System.Drawing.Point(0, 0);
             this.pictureBox_faixa.Name = "pictureBox_faixa";
-            this.pictureBox_faixa.Size = new System.Drawing.Size(1777, 50);
+            this.pictureBox_faixa.Size = new System.Drawing.Size(1777, 40);
             this.pictureBox_faixa.TabIndex = 1;
             this.pictureBox_faixa.TabStop = false;
             // 
@@ -47,11 +49,30 @@
             this.label_razaosocial.AutoSize = true;
             this.label_razaosocial.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.label_razaosocial.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_razaosocial.Location = new System.Drawing.Point(516, 9);
+            this.label_razaosocial.Location = new System.Drawing.Point(751, 9);
             this.label_razaosocial.Name = "label_razaosocial";
             this.label_razaosocial.Size = new System.Drawing.Size(280, 20);
             this.label_razaosocial.TabIndex = 2;
             this.label_razaosocial.Text = "CRIAR SISTEMAS DE SEGURANÇA";
+            // 
+            // panel_cliente
+            // 
+            this.panel_cliente.Location = new System.Drawing.Point(0, 88);
+            this.panel_cliente.Name = "panel_cliente";
+            this.panel_cliente.Size = new System.Drawing.Size(1777, 857);
+            this.panel_cliente.TabIndex = 3;
+            this.panel_cliente.Visible = false;
+            this.panel_cliente.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_cliente_Paint);
+            // 
+            // button_clientes
+            // 
+            this.button_clientes.Location = new System.Drawing.Point(12, 46);
+            this.button_clientes.Name = "button_clientes";
+            this.button_clientes.Size = new System.Drawing.Size(75, 23);
+            this.button_clientes.TabIndex = 4;
+            this.button_clientes.Text = "Clientes";
+            this.button_clientes.UseVisualStyleBackColor = true;
+            this.button_clientes.Click += new System.EventHandler(this.button1_Click);
             // 
             // menu
             // 
@@ -59,8 +80,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1776, 888);
             this.ControlBox = false;
+            this.Controls.Add(this.panel_cliente);
             this.Controls.Add(this.label_razaosocial);
             this.Controls.Add(this.pictureBox_faixa);
+            this.Controls.Add(this.button_clientes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "menu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -76,5 +99,7 @@
         #endregion
         private System.Windows.Forms.PictureBox pictureBox_faixa;
         private System.Windows.Forms.Label label_razaosocial;
+        private System.Windows.Forms.Panel panel_cliente;
+        private System.Windows.Forms.Button button_clientes;
     }
 }
--- gerenciamento/Gerenciador/USUARIO/Menu.cs
+++ gerenciamento/Gerenciador/USUARIO/Menu.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
+
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,15 +20,28 @@ namespace Gerenciador.USUARIO
             this.Width = Screen.PrimaryScreen.Bounds.Width;
             InitializeComponent();
             pictureBox_faixa.Width = Screen.PrimaryScreen.Bounds.Width;
+            panel_cliente.Width = pictureBox_faixa.Width;
             label_razaosocial.Width = pictureBox_faixa.Width / 2;
             label_razaosocial.Height = pictureBox_faixa.Height / 2;
-            
-
+            int x = this.label_razaosocial.Text.Length;
+            x *= 5;
+            this.label_razaosocial.Location = new System.Drawing.Point(Screen.PrimaryScreen.Bounds.Width/2-x, 9);
         }
 
         private void Menu_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void panel_cliente_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel_cliente.Visible = true;
+            panel_cliente.Location = new Point(0, 40);
         }
     }
 }
